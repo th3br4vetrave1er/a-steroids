a789007e11ae20f07cf0b389816926cccc3b8654
th3br4vetrave1er <tazdingodan@gmail.com>
2025-03-29 02:08:07 +0200

    gamedev at its finest l o l

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pygame
+from constants import *
+
+screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.Surface.fill(screen, color='black')  # put here screen var and color
+    pygame.display.flip()
 
 
 def main():
+    pygame.init()
     print('Starting Asteroids!')
-
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
 
 
 if __name__ == "__main__":
