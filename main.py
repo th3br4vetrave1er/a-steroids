e7679def1f04280aa1c7c51303c8fb178ad68163
th3br4vetrave1er <tazdingodan@gmail.com>
2025-03-30 23:25:27 +0300

    brain fog

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
 from constants import *
+from player import Player
+
 
 screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
 running = True
 clock = pygame.time.Clock()
+player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 dt = 0
 
 while running:
@@ -12,7 +15,9 @@ while running:
             running = False
 
     pygame.Surface.fill(screen, color='black')  # put here screen var and color
+    player.draw(screen)
     pygame.display.flip()
+    player.update(dt)
     dt = clock.tick(60) / 1000
 
 
