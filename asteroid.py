85a59582138caa263dcf4e7ccec969d0000e224f
th3br4vetrave1er <tazdingodan@gmail.com>
2025-04-01 18:56:20 +0300

    succesfully failed

--- asteroid.py
+++ asteroid.py
@@ -1,8 +1,10 @@
 import pygame
-from circleshape import Circleshape
+import random
+from constants import *
+from circleshape import CircleShape
 
 
-class Asteroid(Circleshape):
+class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
@@ -11,3 +13,21 @@ class Asteroid(Circleshape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        # randomize the angle of the split
+        random_angle = random.uniform(20, 50)
+
+        a = self.velocity.rotate(random_angle)
+        b = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = a * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = b * 1.2
--- circleshape.py
+++ circleshape.py
@@ -1,22 +1,24 @@
 import pygame
 
 
-# So this is gonna be base class for game objects
-class Circleshape(pygame.sprite.Sprite):
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        if hasattr(self, 'containers'):
+        if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
             super().__init__()
-
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
     def draw(self, screen):
-        # sub-classes must override
-        ...
+        # must override
+        pass
 
     def update(self, dt):
-        # sub-classes must override
-        ...
+        # must override
+        pass
+
+    def collides_with(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
